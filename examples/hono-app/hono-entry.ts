4362dfe061a5e35d2c874ca98485c537e34369e4
Joël Charles <joel.charles91@gmail.com>
2025-03-06 21:32:54 +0100

    chore(hono-example): move api handler in +middleware

--- examples/hono-app/hono-entry.ts
+++ examples/hono-app/hono-entry.ts
@@ -1,20 +1,13 @@
-// import { createHandler } from "@universal-middleware/hono";
-// import { createTodoHandler } from "./server/create-todo-handler.js";
 import { Hono } from "hono";
 import { apply } from "vike-cloudflare/hono";
 
-// FIXME move to +middleware
-// app.post('/api/todo/create', createHandler(() => createTodoHandler)())
-
 function startServer() {
-  const app = new Hono();
-  const port = process.env.PORT || 3000;
+  const app = new Hono()
+  const port = process.env.PORT || 3000
 
-  const { serve } = apply(app);
+  const { serve } = apply(app)
 
-  console.log(process.env.VIKE_RUNTIME);
-
-  return serve({ port: +port });
+  return serve({ port: +port })
 }
 
-export default startServer();
+export default startServer()
