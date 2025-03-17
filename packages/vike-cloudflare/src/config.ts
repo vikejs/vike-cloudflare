778248781dc03798ecf02f1e454338393144296e
Joël Charles <joel.charles91@gmail.com>
2025-03-17 12:53:46 +0100

    format

--- packages/vike-cloudflare/src/config.ts
+++ packages/vike-cloudflare/src/config.ts
@@ -1,40 +1,40 @@
-export { config as default }
+export { config as default };
 
-import { pages as plugin } from './plugins'
-import type { Config } from 'vike/types'
+import { pages as plugin } from "./plugins";
+import type { Config } from "vike/types";
 
 const config = {
-  name: 'vike-cloudflare',
+  name: "vike-cloudflare",
   require: {
-    vike: '>=0.4.225'
+    vike: ">=0.4.225",
   },
   vite: {
-    plugins: [...plugin()]
+    plugins: [...plugin()],
   },
-  extends: ['import:vike-server/config'],
+  extends: ["import:vike-server/config"],
   prerender: {
     value: null,
-    disableAutoRun: true
+    disableAutoRun: true,
   },
   server: {
-    entry: 'virtual:vike-cloudflare:auto-entry',
+    entry: "virtual:vike-cloudflare:auto-entry",
     // We're using rollup's noExternal instead
     // @ts-ignore
-    standalone: false
-  }
-} satisfies Config
+    standalone: false,
+  },
+} satisfies Config;
 
 declare global {
   namespace Vike {
     interface Config {
       server?: {
-        entry: string
-      }
+        entry: string;
+      };
     }
     interface ConfigResolved {
       server?: {
-        entry: string
-      }[]
+        entry: string;
+      }[];
     }
   }
 }
