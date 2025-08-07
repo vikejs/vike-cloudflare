3c2bbda14a72d1250c1c2ce55116940507b9cfba
Romuald Brillout <git@brillout.com>
2025-08-07 12:14:50 +0200

    minor refactor

--- examples/hattip-app/pages/+config.ts
+++ examples/hattip-app/pages/+config.ts
@@ -2,9 +2,8 @@ import vikeSolid from "vike-solid/config";
 import vikeCloudflare from "vike-cloudflare/config";
 import type { Config } from "vike/types";
 import Head from "../layouts/HeadDefault.js";
-import Layout from "../layouts/LayoutDefault.js"; // Default config (can be overridden by pages)
+import Layout from "../layouts/LayoutDefault.js";
 
-// Default config (can be overridden by pages)
 export default {
   Layout,
   Head,
--- examples/hono-app/pages/+config.ts
+++ examples/hono-app/pages/+config.ts
@@ -4,11 +4,9 @@ import type { Config } from "vike/types";
 import Head from "../layouts/HeadDefault.js";
 import Layout from "../layouts/LayoutDefault.js";
 
-// Default config (can be overridden by pages)
 export default {
   Layout,
   Head,
-  // <title>
   title: "My Vike App",
   server: {
     entry: "hono-entry.ts",
